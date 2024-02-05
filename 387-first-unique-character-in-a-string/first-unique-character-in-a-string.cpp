eea1208727ca4f81de43bd7f71ea9533e98a5f66
nitesh052000 <130399175+nitesh052000@users.noreply.github.com>
2024-02-05 15:31:52 +0530

    Time: 19 ms (90.75%) | Memory: 11.9 MB (27.34%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char,int>mp1;
-        map<char,int>mp2;
-        int mini = -1;
-
+        vector<int>v(26,0);
+        for(char c:s){
+            v[c-'a']++;
+        }
         for(int i=0;i<s.length();i++){
-             char ch = s[i];
-             mp1[ch]++;
-             mp2[ch] = i;
+            if(v[s[i]-'a']==1)
+            return i;
         }
-           
-    for(auto it:mp1){
-        if(it.second==1){
-             if(mini==-1){
-                 mini = mp2[it.first];
-             } else{
-                 if(mini>mp2[it.first])
-                 mini = mp2[it.first];
-             }
-        }
-    }
-       return mini;
+        return -1;
     }
 };
