fff12bb63406d7327399a81471f18c8780b7063c
nitesh052000 <130399175+nitesh052000@users.noreply.github.com>
2024-02-12 12:35:40 +0530

    Time: 15 ms (53.21%) | Memory: 22.1 MB (24.81%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        int n = nums.size();
+         map<int,int>mp;
+         for(auto it:nums){
+             mp[it]++;
+         }
+
+         for(auto it:mp){
+             if(it.second>n/2)
+             return it.first;
+         }
+         return -1;
+    }
+};
