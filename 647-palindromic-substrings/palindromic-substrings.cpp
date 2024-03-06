bed01c4b38ce3efac06c67a25fe32dc21f08fb17
nitesh052000 <130399175+nitesh052000@users.noreply.github.com>
2024-03-07 00:56:34 +0530

    Time: 3 ms (82.33%) | Memory: 21.2 MB (28.20%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
-            
-        // 1 - direct + memo;
-        //2 recursion - even +odd;
-        // 3 blueprint
 
-        int n = s.length();
-        int cnt = 0;
-        vector<vector<int>>t(n,vector<int>(n,false));
+    int palindrome(int left,int right,string s){
 
-        for(int l = 1;l<=n;l++){
-          
-            for(int i=0;i+l-1<n;i++){
+        int count= 0;
 
-                int j = i+l-1;
-
-                 if(i==j)
-                  t[i][j] = true;
-                  else if(i+1==j)
-                  t[i][j] = (s[i]==s[j]);
-                  else
-                  t[i][j] = (s[i]==s[j] && t[i+1][j-1]);
-
-                  if(t[i][j]==true)
-                  cnt++;
-                 
-            }
+        while(left>=0 && right<s.length() && s[left]==s[right]){
+            count++;
+            left--;
+            right++;
         }
-        return cnt;
+        return count;
+    }
+
+
+
+    int countSubstrings(string s) {
+        
+        int n = s.length() , ans = 0;
+        
+        for(int i=0;i<n;i++){
+          
+          int odd = palindrome(i,i,s);
+          int even = palindrome(i,i+1,s);
+
+          ans +=odd+even;
+
+        }
+        return ans;
     }
 };
