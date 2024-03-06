72375c3fd02e25abc07270fcd4679348de44774e
nitesh052000 <130399175+nitesh052000@users.noreply.github.com>
2024-03-07 00:46:30 +0530

    Time: 29 ms (28.70%) | Memory: 32.8 MB (17.94%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-
-    bool ispalindrome(string str){
-         int i = 0;
-         int j = str.length()-1;
-
-         while(i<j){
-             if(str[i]==str[j]){
-                 i++;
-                 j--;
-             }
-            else
-            return false;
-         }
-         return true;
-    }
-
     int countSubstrings(string s) {
-        
+            
+        // 1 - direct + memo;
+        //2 recursion - even +odd;
+        // 3 blueprint
+
         int n = s.length();
         int cnt = 0;
+        vector<vector<int>>t(n,vector<int>(n,false));
 
-        for(int i=0;i<n;i++){
-            string str = "";
-            for(int j=i;j<n;j++){
-                str = str + s[j];
-                if(ispalindrome(str))
-                cnt++;
+        for(int l = 1;l<=n;l++){
+          
+            for(int i=0;i+l-1<n;i++){
+
+                int j = i+l-1;
+
+                 if(i==j)
+                  t[i][j] = true;
+                  else if(i+1==j)
+                  t[i][j] = (s[i]==s[j]);
+                  else
+                  t[i][j] = (s[i]==s[j] && t[i+1][j-1]);
+
+                  if(t[i][j]==true)
+                  cnt++;
+                 
             }
         }
         return cnt;
