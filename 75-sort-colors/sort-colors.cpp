1a1e41720346034061d48f70aa207d8d64aaf24b
nitesh052000 <130399175+nitesh052000@users.noreply.github.com>
2024-04-16 03:10:32 +0530

    Time: 4 ms (30.90%) | Memory: 9.7 MB (72.33%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -17,14 +17,30 @@ public:
             two++;
         }
 
-        for(int i=0;i<zero;i++){
-            nums[i] = 0;
-        }
-        for(int i=zero;i<one+zero;i++){
-            nums[i] = 1;
-        }
-        for(int i=one+zero;i<two+one+zero;i++){
-            nums[i] = 2;
+        // for(int i=0;i<zero;i++){
+        //     nums[i] = 0;
+        // }
+        // for(int i=zero;i<one+zero;i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i=one+zero;i<two+one+zero;i++){
+        //     nums[i] = 2;
+        // }
+
+        for(int i=0;i<nums.size();i++){
+
+             if(zero>0){
+                nums[i]=0;
+                zero--;
+             }
+             else if(one>0){
+                nums[i]=1;
+                one--;
+             }
+             else{
+                nums[i]=2;
+             }
+
         }
 
     }
